1c13cc6f7598ef7f653a85b7f0ab7b350f1b7e46
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2018-12-29 22:16:09 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,16 +5,17 @@ module.exports = {
     usage: 'help',
     guildOnly: false,
     execute(message, args) {
-      const exampleEmbed = new Discord.RichEmbed()
-    .setColor('#00ff00')
-    .setTitle('UseBot Help')
-    .setURL('https://discord.js.org/')
-    .setAuthor('UseBot', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64', 'https://discord.js.org')
-    .setDescription('List of commands')
-    .setThumbnail('https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64')
-    .addField('Commands', 'U.ping U.avatar U.beep U.prefix U.server U.hug U.say U.user-info U.help U.ban <reason> (BOT OWNER ONLY) U.kick <reason> (BOT OWNER ONLY) U.renamebot (BOT OWNER ONLY).', true)
-    .setTimestamp()
-    .setFooter('The prefix is "U." for your pleasure.', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64');
-message.channel.send(exampleEmbed);
+//      const exampleEmbed = new Discord.RichEmbed()
+//    .setColor('#00ff00')
+//    .setTitle('UseBot Help')
+//    .setURL('https://discord.js.org/')
+//    .setAuthor('UseBot', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64', 'https://discord.js.org')
+//    .setDescription('List of commands')
+//    .setThumbnail('https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64')
+//    .addField('Commands', 'U.ping U.avatar U.beep U.prefix U.server U.hug U.say U.user-info U.help U.ban <reason> (BOT OWNER ONLY) U.kick <reason> (BOT OWNER ONLY) U.renamebot (BOT OWNER ONLY).', true)
+//    .setTimestamp()
+//    .setFooter('The prefix is "U." for your pleasure.', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64');
+//message.channel.send(exampleEmbed);
+       message.channel.send("**Commands**, 'U.ping U.avatar U.beep U.prefix U.server U.hug U.say U.user-info U.help U.ban <reason> (BOT OWNER ONLY) U.kick <reason> (BOT OWNER ONLY) U.renamebot (BOT OWNER ONLY).");
     },
 };
