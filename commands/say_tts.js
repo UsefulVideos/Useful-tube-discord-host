65a13c6806cac7105fe10df713db644055b82c41
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2018-12-30 12:56:23 +0200

    Create say_tts.js

---
+++ commands/say_tts.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'say_tts',
+    description: 'Say TTS!',
+    guildOnly: true,
+    execute(message, args) {
+      let text = args.join(" ");
+      message.delete();
+      message.reply((text), {tts: true});
+  },
+
+};
