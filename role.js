958706a7fc11f32eb68f96be1f449a1ac6969cfb
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2018-12-30 10:05:12 +0200

    Add files via upload

---
+++ role.js
@@ -0,0 +1,8 @@
+if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+    return message.channel.send(reply);
+}
