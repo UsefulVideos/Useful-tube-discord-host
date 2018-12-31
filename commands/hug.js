ef3a2f905b23f495595e459c5d5f7fd91a25f20b
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2018-12-31 22:59:19 +0200

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -6,13 +6,14 @@ module.exports = {
         if (!message.mentions.users.size) {
         return message.reply('you need to tag a user in order to hug them!');
 }
-        if (message.author !== message.mentions.members.first() ) {
+        if (message.author === message.mentions.members.first() ) {
+        message.channel.send(`Sorry ${message.author}! But you cannot hug yourself!n/Try again with a different member. :frowning2:`);
+		message.delete();
+        }
+        else
         let member = message.mentions.members.first();
         message.channel.send(`:heart: Hey ${member}! ${message.author} sent you a hug. :heart:`, {files: ["https://media.giphy.com/media/8tpiC1JAYVMFq/giphy.gif"]});
         message.delete();
-        }
-        else message.channel.send(`Sorry ${message.author}! But you cannot hug yourself!n/Try again with a different member. :frowning2:`);
-		message.delete();
         
     },
 };
