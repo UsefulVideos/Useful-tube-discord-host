11527d1ce0ee45ee478446c64f0daa9437f89841
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-02 12:05:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.once('ready', () => {
         console.log(" - " + guild.name)
     });
     //client.setActivity(`Serving ${client.guilds.size} servers`);
-    client.user.setGame('Use U.help for commands.')
+    client.user.setActivity('Use U.help for commands.')
 });
 
 client.on('message', message => {
