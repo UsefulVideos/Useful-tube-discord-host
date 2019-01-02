cea85fbbe6a4bf554175d7eb05354b3d771fe306
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-02 23:08:19 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,18 +6,18 @@ module.exports = {
     usage: 'help',
     guildOnly: false,
     execute(message, args) {
-//      const exampleEmbed = new Discord.RichEmbed()
-//    .setColor('#00ff00')
-//    .setTitle('UseBot Help')
-//    .setURL('https://discord.js.org/')
-//    .setAuthor('UseBot', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64', 'https://discord.js.org')
-//    .setDescription('List of commands')
-//    .setThumbnail('https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64')
-//    .addField('Commands', 'U.ping U.avatar U.beep U.prefix U.server U.hug U.say U.user-info U.help U.ban <reason> (BOT OWNER ONLY) U.kick <reason> (BOT OWNER ONLY) U.renamebot (BOT OWNER ONLY).', true)
-//    .setTimestamp()
-//    .setFooter('The prefix is "U." for your pleasure.', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64');
-//message.channel.send(exampleEmbed);
-       message.channel.send("**Commands:**");
-       message.channel.send(`\```${prefix}ping \n${prefix}avatar \n${prefix}beep \n${prefix}prefix \n${prefix}server \n${prefix}hug \n${prefix}say <content> \n${prefix}user-info \n${prefix}help \n${prefix}ban <reason> (BOT OWNER ONLY) \n${prefix}kick <reason> (BOT OWNER ONLY) \n${prefix}renamebot (BOT OWNER ONLY)\````);
+      const exampleEmbed = new Discord.RichEmbed()
+     .setColor('#00ff00')
+     .setTitle('UseBot Help')
+     .setURL('https://discord.js.org/')
+     .setAuthor('UseBot', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64', 'https://discord.js.org')
+     .setDescription('List of commands')
+     .setThumbnail('https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64')
+     .addField('Commands', `\```https://stackoverflow.com/questions/49759835/welcome-message-when-joining-discord-server-using-discord-jsping \n${prefix}avatar \n${prefix}beep \n${prefix}prefix \n${prefix}server \n${prefix}hug \n${prefix}say <content> \n${prefix}user-info \n${prefix}help \n${prefix}ban <reason> (BOT OWNER ONLY) \n${prefix}kick <reason> (BOT OWNER ONLY) \n${prefix}renamebot (BOT OWNER ONLY)\``` `, true)
+    .setTimestamp()
+     .setFooter('The prefix is "U." for your pleasure.', 'https://cdn.discordapp.com/app-icons/347332147591512064/8dd5e1f4a7d4507328f49747ca558d54.png?size=64');
+message.channel.send(exampleEmbed);
+//       message.channel.send("**Commands:**");
+//       message.channel.send(` \```https://stackoverflow.com/questions/49759835/welcome-message-when-joining-discord-server-using-discord-jsping \n${prefix}avatar \n${prefix}beep \n${prefix}prefix \n${prefix}server \n${prefix}hug \n${prefix}say <content> \n${prefix}user-info \n${prefix}help \n${prefix}ban <reason> (BOT OWNER ONLY) \n${prefix}kick <reason> (BOT OWNER ONLY) \n${prefix}renamebot (BOT OWNER ONLY)\``` `);
     },
 };
