b9351fca9c5e8ceb3747809aa8651cb4eac80bf0
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-05 20:59:29 +0200

    Update quack.js

--- commands/quack.js
+++ commands/quack.js
@@ -13,7 +13,7 @@ module.exports = {
         message.channel.send("**What do natural c-curve nails and duck beaks have the most in common?**");
         message.channel.send("**A.** Their color. Their color is similar. \n**B.** Their thickness. It's sorta alike.\n**C.** Their shape. Duck beaks and c-curve nails resemble almost the same shape.\n**D.** Their glossiness. Their glossiness or shininess is much alike.\n**E.** Their material and texture. Their material is something solid that can be cut but doesn't hurt.\n**F.** Their fluids. They are both made of the same fluid that the animal body depends on.");
         message.channel.send(`To answer the question, please type in this form: \n**${second_prefix}answer <letter>** `);
-        if (message.content === second_prefix + "answer E") {
+        if (message.content === second_prefix + "answer" + correctletter) {
          message.channel.send("Congrats! For getting the correct answer, **E**!");
          }
        else if (message.content === second_prefix + "answer" + incorrectletter) {
