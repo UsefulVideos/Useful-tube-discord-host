f689fd3897cf21a665dcbd2133a415d14958d81e
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-05 20:58:51 +0200

    Update quack.js

--- commands/quack.js
+++ commands/quack.js
@@ -1,5 +1,7 @@
-const prefix = process.env.PREFIX;
+const second_prefix = process.env.SECOND_PREFIX;
 const incorrectletter = ["A", "B", "C", "D", "F"];
+const letters = ["A", "B", "C", "D", "E", "F"];
+const correctletter = ["E"];
 module.exports = {
     name: 'quack',
     description: 'Quack',
@@ -10,13 +12,12 @@ module.exports = {
         message.channel.send("If you did, then please answer this question correctly:");
         message.channel.send("**What do natural c-curve nails and duck beaks have the most in common?**");
         message.channel.send("**A.** Their color. Their color is similar. \n**B.** Their thickness. It's sorta alike.\n**C.** Their shape. Duck beaks and c-curve nails resemble almost the same shape.\n**D.** Their glossiness. Their glossiness or shininess is much alike.\n**E.** Their material and texture. Their material is something solid that can be cut but doesn't hurt.\n**F.** Their fluids. They are both made of the same fluid that the animal body depends on.");
-        message.channel.send(`To answer the question, please type in this form: \n**${prefix}answer <letter>** `);
-        if (message.content.startsWith(prefix + "answer E")) {
+        message.channel.send(`To answer the question, please type in this form: \n**${second_prefix}answer <letter>** `);
+        if (message.content === second_prefix + "answer E") {
          message.channel.send("Congrats! For getting the correct answer, **E**!");
          }
-       else if (message.content.startsWith(prefix + "answer" + incorrectletter)) {
-        message.channel.send("Incorrect, the correct answer to the question is actually **E**!");
-        message.channel.send("I guess you did not realise what nails and duck beaks have the most in common.");
+       else if (message.content === second_prefix + "answer" + incorrectletter) {
+        message.channel.send("Incorrect, the correct answer to the question is actually **E**! \nI guess you did not realise what nails and duck beaks have the most in common.");
        }
     },
 };
