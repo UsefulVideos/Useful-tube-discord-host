a34c5336aa2309e0e8e8d11744055e576c5e1727
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-05 19:22:46 +0200

    Update no-command.js

--- commands/no-command.js
+++ commands/no-command.js
@@ -1,7 +1,8 @@
+const prefix = process.env.PREFIX;
 module.exports = {
     name: 'no-command',
     description: 'No commands!',
     execute(message, args) {
-        return message.channel.send(`You didn't provide any arguments, ${message.author}! Use help for more commands.`);
+        return message.channel.send(`You didn't provide any command, ${message.author}! Use ${prefix}help for more commands.`);
     },
 };
