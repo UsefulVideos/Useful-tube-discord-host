d90a5795b8854278b259d7cc4d52e39f9ddb96bb
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-05 19:28:09 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,10 @@ module.exports = {
     usage: 'help',
     guildOnly: false,
     execute(message, args) {
-       message.channel.send("**Commands:**");
+       message.channel.send("**Regular Commands:**");
        message.channel.send(" ```U.ping \nU.avatar \nU.beep \nU.prefix \nU.server \nU.hug \nU.say <content> \nU.user-info \nU.help \nU.ban <reason> (BOT OWNER ONLY) \nU.kick <reason> (BOT OWNER ONLY) \nU.renamebot (BOT OWNER ONLY)``` ");
+       message.channel.send("**Random Commands:**");
+       message.channel.send(" ```U.quack \nU.holographic \nU.hello \nU.options``` "); 
+       message.channel.send("*In case of issues, please contact the bot owner. Thank you for reading!*");
     },
 };
