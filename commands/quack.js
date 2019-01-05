820cef4451acd52e245c27439a3e385ca263a988
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-05 20:31:45 +0200

    Update quack.js

--- commands/quack.js
+++ commands/quack.js
@@ -1,4 +1,5 @@
 const prefix = process.env.PREFIX;
+const incorrectletter = 'A','B','C','D';
 module.exports = {
     name: 'quack',
     description: 'Quack',
@@ -8,18 +9,13 @@ module.exports = {
         message.channel.send("But have you ever heard of a human body part that looks similar to the beak of a duck, don't you?");
         message.channel.send("If you did, then please answer this question correctly:");
         message.channel.send("**What do natural c-curve nails and duck beaks have the most in common?**");
-        message.channel.send("**A.** Their color. Their color is similar.");
-        message.channel.send("**B.** Their thickness. It's sorta alike.");
-        message.channel.send("**C.** Their shape. Duck beaks and c-curve nails resemble almost the same shape.");
-        message.channel.send("**D.** Their glossiness. Their glossiness or shininess is much alike.");
-        message.channel.send("**E.** Their material and texture. Their material is something solid that can be cut but doesn't hurt.");
-        message.channel.send("**D.** Their fluids. They are both made of the same fluid that the animal body depends on.");
+        message.channel.send("**A.** Their color. Their color is similar. \n**B.** Their thickness. It's sorta alike.\n**C.** Their shape. Duck beaks and c-curve nails resemble almost the same shape.\n**D.** Their glossiness. Their glossiness or shininess is much alike.\n**E.** Their material and texture. Their material is something solid that can be cut but doesn't hurt.\n**E.** Their fluids. They are both made of the same fluid that the animal body depends on.");
         message.channel.send(`To answer the question, please type in this form: \```${prefix}answer <letter>````);
         if (message.content.startsWith(prefix + "answer E")) {
          message.channel.send("Congrats! For getting the correct answer, **E**!");
-       }
-       else {
-        message.channel.send("I am sorry, but the correct answer to the question is actually **D**!");
+         }
+       else if (message.content.startsWith(prefix + "answer" + incorrectletter)) {
+        message.channel.send("Incorrect, the correct answer to the question is actually **E**!");
         message.channel.send("I guess you did not realise what nails and duck beaks have the most in common.");
        }
     },
