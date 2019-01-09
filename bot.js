08814fc10dab854b1e4b4f899c6fde0260f1ec8f
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-09 08:35:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,10 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     
+    if (command.guildOnly && message.channel.type !== 'text') {
+    return message.reply('I can\'t execute that command inside DMs!');
+}
+    
     if (command === `ping`) {
     client.commands.get('ping').execute(message, args);
 }
