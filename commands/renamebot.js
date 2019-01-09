376804c78d226afc61a10d22caff977e89e3d2c8
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-09 08:20:27 +0200

    Update renamebot.js

--- commands/renamebot.js
+++ commands/renamebot.js
@@ -1,1 +1,17 @@
-
+const client = new Discord.Client();
+const ownerid = process.env.OWNER_ID;
+module.exports = {
+    name: 'renamebot',
+    description: 'Renames the bot!',
+    guildOnly: false,
+    execute(message, args) {
+        if (message.author.id === ownerid) {
+  let text = args.join(" ");
+  client.user.setUsername(text);
+  message.delete();
+  }
+  else
+  message.channel.send(`**BOT OWNER ONLY!**`);
+}
+    },
+};
