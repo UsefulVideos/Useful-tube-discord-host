eb6b411365fb2dd82b74fb573c77f29bbd873a5c
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-12 19:15:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || !message.content.startsWith(sprefix) || message.author.bot) return;  
+    if (!message.content.startsWith(prefix, sprefix) || message.author.bot) return;  
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     
