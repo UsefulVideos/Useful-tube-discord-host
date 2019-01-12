f4e31f92fdcd3afc44e88703100d1df01cf2b3a5
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-13 00:44:38 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,9 +1,10 @@
+const prefix = process.env.PREFIX;
 module.exports = {
     name: 'prefix',
     description: 'The bot prefix!',
     usage: `Shows the bot's command prefix.`,
     guildOnly: true,
     execute(message, args) {
-     message.channel.send(`${message.author} My prefix is "U.". The abillity to add custom prefixes will come soon.`);
+     message.channel.send(`${message.author} My prefix is "${prefix}". The abillity to add custom prefixes will come soon.`);
     },
 };
