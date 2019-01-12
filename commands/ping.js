9dbcdd80a46fb1ca57c0e06509918cb9ea26c204
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-12 20:34:18 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(message, args) {
         let user = message.mentions.users.first();
         if (user) {
-        message.channel.send(`${message.author}, it seems that you pinged ` + user.);
+        message.channel.send(`${message.author}, it seems that you pinged ${user}.`);
         } else
         if (!user) {
 message.channel.send("Please provide a valid user to mention, otherwise i don't understand how to help you! :worried:");
