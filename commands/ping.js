5ccbbdd3d40b6ddab7acf44a46a730c0125a10ed
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-12 20:41:51 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,11 +4,14 @@ module.exports = {
     guildOnly: true,
     execute(message, args) {
         let user = message.mentions.users.first();
-        if (user) {
+        if (user != message.author) {
         message.channel.send(`${message.author}, it seems that you pinged ${user}.`);
         } else
         if (!user) {
-message.channel.send("Please provide a valid user to mention, otherwise i don't understand how to help you! :worried:");
+message.channel.send("Please provide a valid user to mention, otherwise i don't understand how to help you. :worried:");
+        } else
+        if (user == message.author) {
+        message.channel.send(`${message.author}, i am sorry but you cannot execute this command to yourself.  :worried:`);
         }
     },
 };
