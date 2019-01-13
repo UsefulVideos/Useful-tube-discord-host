fbd237784691dc505ecf784c9927213e5b956330
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-13 10:58:37 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'kick user',
     guildOnly: true,
     execute(message, args) {
-      if (!message.mentions.users.size) {
+      if (!message.mentions.members.size) {
       return message.reply('You need to tag a user in order to kick them!');
 }
       if (message.author.id === '251050176427327499') {
@@ -13,6 +13,8 @@ module.exports = {
       return message.channel.send(`${member} got kicked from the server for: "${reason}".`);   
   }
       else message.channel.send(`You do not have permission to kick ${member}.`);
-
+      if (!reason) {
+      return message.channel.send(`I am sorry but you cannot kick the user without specifying the reason! :worried:`);   
+     }
   },
 };
