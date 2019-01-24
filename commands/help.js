7193f381deb286961ab9717aae76b86923882764
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-24 17:16:39 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,6 +13,6 @@ module.exports = {
        //message.channel.send("*In case of issues, please contact the bot owner. Thank you for reading!*");
        
        const Embed = new Discord.RichEmbed().setTitle('PresentBot Help').setDescription('Welcome to the list of commands!').addField('**Regular Commands**', true).addBlankField().addField('P.ping \nP.avatar \nP.beep \nP.prefix \nP.server \nP.hug \nP.say <content> \nP.user-info \nP.help \nP.ban <reason> (BOT OWNER ONLY) \nP.kick <reason> (BOT OWNER ONLY) \nP.renamebot (BOT OWNER ONLY)', true).addBlankField().addField('**Random Commands**', true).addField('P.quack \nP.holographic \nP.hello \nP.options', true).setFooter('*In case of issues, please contact the bot owner. Thank you for reading!*');
-        message.channel.send(Embed)
+        message.channel.send(Embed);
     },
 };
