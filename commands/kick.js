19bc4a4109a6062766f13bb300e28a1ab41470a1
UsefulVideos <46186498+UsefulVideos@users.noreply.github.com>
2019-01-26 12:17:33 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -12,14 +12,16 @@ module.exports = {
   }   else
       if (member.hasPermission('KICK_MEMBERS')) {
       let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-      if(!member)
-      return message.channel.send("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.channel.send("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
       let reason = args.slice(1).join(" ");
       if(!reason) reason = "No reason provided";
       member.kick(reason);
       message.channel.send(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+      if(!member) {
+      return message.channel.send("Please mention a valid member of this server");
+       }
+    if(!member.kickable)  {
+      return message.channel.send("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+     }
     }
   },
 };
